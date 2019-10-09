8e20a477f5fc6ee509f5250efe22b7b8792ba2ea
Rooseveltiny <mister.savvvik@gmail.com>
2019-10-09 09:54:19 +0300

    changed wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 import sys, os
+# sys.path.append('/home/r/roosevelt/keklol.ru/HelloFlask') # указываем директорию с проектом
+# sys.path.append('/home/r/roosevelt/.local/lib/python3.6/site-packages') # указываем директорию с библиотеками, куда поставили Flask
+
 sys.path.append('/home/r/roosevelt/keklol.ru/HelloFlask') # указываем директорию с проектом
-sys.path.append('/home/r/roosevelt/.local/lib/python3.6/site-packages') # указываем директорию с библиотеками, куда поставили Flask
+sys.path.append('./env/lib/python3.6/site-packages') # указываем директорию с библиотеками, куда поставили Flask
+
 from HelloFlask import app as application # когда Flask стартует, он ищет application. Если не указать 'as application', сайт не заработает
 from werkzeug.debug import DebuggedApplication # Опционально: подключение модуля отладки
 application.wsgi_app = DebuggedApplication(application.wsgi_app, True) # Опционально: включение модуля отадки
