1ec3dab160e91ce4c8cfe56dcf4f916ca0dc0864
Rooseveltiny <mister.savvvik@gmail.com>
2019-10-09 20:53:58 +0300

    fixed bug when vk bot doesn't work

--- HelloFlask/perform_bot.py
+++ HelloFlask/perform_bot.py
@@ -21,6 +21,25 @@ def take_care_about_call(req: 'request')->'performin bot action':
         return make_a_respond(data)
 
 
+def make_a_respond(data):
+
+
+    message = data['object']['body']
+
+    session = vk.Session()
+    api = vk.API(session, v=5.0)
+    user_id = data['object']['user_id']
+
+
+    day = get_day(message)
+
+    schedule = get_schedule(day)  
+
+    api.messages.send(access_token=token, user_id=str(
+                    user_id), message=schedule)
+
+    return 'ok'
+
 def send_schedule():
 
     session = vk.Session()
